c6b32d3f6b623769704441905377efd3b05fe8c7
mehmetugur <u.mehmet@gmail.com>
2018-05-16 09:21:12 -0400

    Updated all files

---
+++ client/main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import App from '/client/imports/App'
+
+Meteor.startup(function(){
+render (
+  <App />,
+  document.getElementById('render-target')
+)
+
+});
